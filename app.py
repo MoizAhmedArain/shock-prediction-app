d3af2749f4df6289ea362f9171eb2b9f6880959e
Moiz Ahmed Arain <153999362+MoizAhmedArain@users.noreply.github.com>
2025-08-17 19:57:57 +0500

    OK app.py

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ plt.legend()
 st.pyplot(fig)
 
 # training / testing split 
-data_training = pd.DataFrame(df["Close"][0:int(len(df) * 0.70)])
-data_testing  = pd.DataFrame(df["Close"][int(len(df) * 0.70):])
+# data_training = pd.DataFrame(df["Close"][0:int(len(df) * 0.70)])
+# data_testing  = pd.DataFrame(df["Close"][int(len(df) * 0.70):])
 
 # scale on training only
 try:
@@ -72,63 +72,63 @@ except Exception as e:
     st.error(f"Scaler Can't be perfromed: {e}")
     st.stop()
     
-data_training_array = scaler.fit_transform(data_training) 
+# data_training_array = scaler.fit_transform(data_training) 
 
-# build x_train, y_train using 100 day 
-x_train = []
-y_train = []
-for i in range(100, data_training_array.shape[0]):
-    x_train.append(data_training_array[i-100:i, 0])   
-    y_train.append(data_training_array[i, 0])
+# # build x_train, y_train using 100 day 
+# x_train = []
+# y_train = []
+# for i in range(100, data_training_array.shape[0]):
+#     x_train.append(data_training_array[i-100:i, 0])   
+#     y_train.append(data_training_array[i, 0])
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))  
+# x_train = np.array(x_train)
+# y_train = np.array(y_train)
+# x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))  
 
-# prepare test input (last 100 days of train + testing)
-past_100_days = data_training.tail(100)
-final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+# # prepare test input (last 100 days of train + testing)
+# past_100_days = data_training.tail(100)
+# final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
 
-input_data = scaler.transform(final_df)  
+# input_data = scaler.transform(final_df)  
 
-x_test = []
-y_test = []
-for i in range(100, input_data.shape[0]):
-    x_test.append(input_data[i-100:i, 0])
-    y_test.append(input_data[i, 0])
+# x_test = []
+# y_test = []
+# for i in range(100, input_data.shape[0]):
+#     x_test.append(input_data[i-100:i, 0])
+#     y_test.append(input_data[i, 0])
 
-x_test = np.array(x_test).reshape((len(x_test), 100, 1))
-y_test = np.array(y_test)
+# x_test = np.array(x_test).reshape((len(x_test), 100, 1))
+# y_test = np.array(y_test)
 
-try:
-    y_predicted = model.predict(x_test)
-except Exception as e:
-    st.error(f"model is not loading: {e}")
-    st.stop()
+# try:
+#     y_predicted = model.predict(x_test)
+# except Exception as e:
+#     st.error(f"model is not loading: {e}")
+#     st.stop()
     
-y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1))
-y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
-# plot
-st.header("Stock Prediction vs Actual Data")
-fig, ax = plt.subplots(figsize=(12,6))
-ax.plot(y_test, "b", label="Original Price")
-ax.plot(y_predicted, "r", label="Predicted Price")
-ax.set_xlabel("Time")
-ax.set_ylabel("Price")
-ax.legend()
-st.pyplot(fig)
+# y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1))
+# y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+# # plot
+# st.header("Stock Prediction vs Actual Data")
+# fig, ax = plt.subplots(figsize=(12,6))
+# ax.plot(y_test, "b", label="Original Price")
+# ax.plot(y_predicted, "r", label="Predicted Price")
+# ax.set_xlabel("Time")
+# ax.set_ylabel("Price")
+# ax.legend()
+# st.pyplot(fig)
 
 #Prediction from 100 days
 da100 = df['Close'].tail(100)
-scalers = MinMaxScaler(feature_range=(0,1))
-scaled100 = scalers.fit_transform(da100) 
+#scalers = MinMaxScaler(feature_range=(0,1))
+scaled100 = scaler.fit_transform(da100) 
 x_input = np.array(scaled100).reshape(1, 100, 1) 
 try:
     pred_scaled = model.predict(x_input)
 except Exception as e:
     st.error(f"model is not performing: {e}")
     st.stop()
-pred_price = scalers.inverse_transform(pred_scaled)
+pred_price = scaler.inverse_transform(pred_scaled)
 actual_prices = df["Close"].tail(100)
 all_prices = actual_prices + [pred_price[0][0]]
 
@@ -141,6 +141,7 @@ ax.set_xlabel("Days")
 ax.set_ylabel("Price")
 ax.legend()
 st.pyplot(fig)
+
 
 
 
